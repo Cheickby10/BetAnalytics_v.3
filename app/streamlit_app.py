9da0b661001f787ee8dbf6356e46e7cca62c2776
Cheickby10 <ccoulibaly873@gmail.com>
2026-02-17 23:00:57 +0000

    Add sys and os imports to streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 from core.environment import CrashEnv
 from core.bayesian import EdgeEstimator
